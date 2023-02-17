b38b920c3a7496f1a2eafa96d80305ef2b1c5da8
gdfuraha <91331165+gdfuraha@users.noreply.github.com>
2023-02-17 10:47:14 +0200

    Update phone_no_form.dart

--- carpool/lib/phone_no_form.dart
+++ carpool/lib/phone_no_form.dart
@@ -1,1 +1,84 @@
+import 'dart:html';
 
+import 'package:flutter/material.dart';
+
+class VerificationPage extends StatefulWidget {
+  const VerificationPage({Key? key}) : super(key: key);
+
+  @override
+  State<VerificationPage> createState() => _VerificationPageState();
+}
+
+class _VerificationPageState extends State<VerificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(children: [
+            SizedBox(height: 200),
+            //Hello again!
+            Text(
+              "Enter your phone number",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Phone Number                               ",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 20),
+
+            //phone number textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "###",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            //sign in button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                    color: Color(0xff921376),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                  child: Text(
+                    "Next",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
